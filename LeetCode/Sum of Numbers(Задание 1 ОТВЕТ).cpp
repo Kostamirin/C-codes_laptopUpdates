b6b54a51aa875f3646ae601a0bf8bfb6257cf303
KerasinChick <base@gmain.com>
2024-12-06 22:08:55 +0300

    update

--- LeetCode/Sum of Numbers(Задание 1 ОТВЕТ).cpp
+++ LeetCode/Sum of Numbers(Задание 1 ОТВЕТ).cpp
@@ -1,4 +1,4 @@
-//#include <iostream>
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 
@@ -19,3 +19,14 @@ public:
         return {}; // возвращаем пустой вектор, если решение не найдено (хотя по условию оно всегда есть)
     }
 };
+
+int main() {
+  Solution solution;
+  std::vector<int> nums = {2, 7, 11, 15};
+  std::vector<int> something = solution.twoSum(nums, 13);
+  for (int i = 0; i < something.size(); i++)
+  {
+      std::cout<<something[i] <<std::endl;
+  }
+  return 0;
+}
