144b9a81d413ba6bb0685ff46b1789fbe2938a0d
KerasinChick <base@gmain.com>
2025-01-26 17:37:46 +0300

    update (Unversioned)

---
+++ 2337.Move Piece to Obtain a String_Answer.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        if (start == target) {
+            return true;
+        }
+        int waitL = 0;
+        int waitR = 0;
+
+        for (int i = 0; i < start.length(); i++) {
+            char curr = start[i];
+            char goal = target[i];
+            if (curr == 'R') {
+                if (waitL > 0) {
+                    return false;
+                }
+                waitR++;
+            }
+            if (goal == 'L') {
+                if (waitR > 0) {
+                    return false;
+                }
+                waitL++;
+            }
+            if (goal == 'R') {
+                if (waitR == 0) {
+                    return false;
+                }
+                waitR--;
+            }
+
+            if (curr == 'L') {
+                if (waitL == 0) {
+                    return false;
+                }
+                waitL--;
+            }
+
+        }
+        return waitL == 0 && waitR == 0;
+    }
+};
+#include <iostream>
+int main(){
+    Solution s;
+    string start = "_L_L_R__R_";
+    string target = "LL____R__R";
+    bool something = s.canChange(start, target);
+    std::cout <<(something? "true": "false") <<std::endl;
+    std::cin.get();
+    return 0;
+}
+
