37a575b207b044a6abae30d9a33ee6f5923c1d57
kley-muner_laptop <kerasin988@gmail.com>
2025-01-30 21:05:03 +0300

    update

---
+++ C++ (personal_works)/helper.cpp
@@ -0,0 +1,67 @@
+//
+// Created by kley-muner on 1/29/25.
+//
+//#include <algorithm>
+#include <vector>
+#include <cmath>
+
+class something{
+public:
+    // функция выдает значение цифры
+    static std::vector<int>  base(int a){
+        if (a < 10){return {a};}
+        else if (a >= 10 && a < 100)
+        {
+            return {static_cast<int>(floor(a/=10)),a%10};
+        }
+        else if (a >=100 && a < 1000)
+        {
+            //! Стоит фиксить
+            return {static_cast<int>(floor(a/=100)),static_cast<int>(floor(a%10)),a%100};
+        }
+        else{std::cout <<"Error"; return {0};}
+    }
+    // функция добавляет необходимое количество цифр, относительно проверенного числа(одного)
+    static std::vector<int> change(std::vector<int> & answer, std::vector<int> base){
+        for (int i = 0; i < base.size(); i++)
+        {
+            switch (base[i])
+            {
+            case 1:
+                answer[0] ++;
+                break;
+            case 2:
+                answer[1] ++;
+                break;
+            case 3:
+                answer[2] ++;
+                break;
+            case 4:
+                answer[3] ++;
+                break;
+            case 5:
+                answer[4] ++;
+                break;
+            case 6:
+                answer[5] ++;
+                break;
+            case 7:
+                answer[6] ++;
+                break;
+            case 8:
+                answer[7] ++;
+                break;
+            case 9:
+                answer[8] ++;
+                break;
+            case 0:
+                answer[9] ++;
+                break;
+            default:
+                break;
+            }
+        }
+        return answer;
+    }
+
+};
--- C++ (personal_works)/Сколько цифр необходимо преобрести относительно количества кабинетов.cpp
+++ C++ (personal_works)/Сколько цифр необходимо преобрести относительно количества кабинетов.cpp
@@ -2,28 +2,44 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;
-class Solution{
-    std::vector<int> first(int *number){
-        something = NULL; new something;
-
-        switch (something)
-        {
-        case 1:
-            break;
-        }
-    }
-};
+#include <cmath>
+#include "helper.cpp"
 
 int main(){
-    int number = 13;
-    int answer = 6;
-    // Далее должен идти код и сравнение результата с ответом
+    something something;
+    int number;
+    std::cin >>number;
+    //int answer = 6;
     // Можно будет создать другие вариации потом
+    if (number <=0){std::cout <<"Error"; return 0;}
+    std::vector<int> answer;
+    for (int i = 0; i <= number; i++)
+    {
+        std::vector<int> base = something.base(i);
+        answer = something.change(answer, base);
+    }
+    int one=0; int two=0; int three=0; int four=0; int five=0;
+    int six=0; int seven=0; int eight=0; int nine=0; int zero=0;
+    for (int i = 0; i < answer.size(); i++)
+    {
+        switch (i)
+        {
+            case 0: one = answer[i]; break;
+            case 1: two = answer[i]; break;
+            case 2: three = answer[i]; break;
+            case 3: four = answer[i]; break;
+            case 4: five = answer[i]; break;
+            case 5: six = answer[i]; break;
+            case 6: seven = answer[i]; break;
+            case 7: eight = answer[i]; break;
+            case 8: nine = answer[i]; break;
+            case 9: zero = answer[i]; break;
+            default: break;
+        }
+    }
 
-    int one, two, three, four, five, six, seven, eight, nine, ziro;
     std::cout <<"One's -- " <<one <<std::endl;
-    std:;cout <<"Two's -- " <<two <<std:: endl;
+    std::cout <<"Two's -- " <<two <<std:: endl;
     std::cout <<"Three's -- " <<three <<std::endl;
     std::cout <<"Four's -- " <<four <<std::endl;
     std::cout <<"Five's -- " <<five <<std::endl;
@@ -31,6 +47,7 @@ int main(){
     std::cout <<"Seven's -- " <<seven <<std::endl;
     std::cout <<"Eight's -- " <<eight <<std::endl;
     std::cout <<"Nine's -- " <<nine <<std::endl;
-    std::cout <<"Ziro's -- " <<ziro <<std::endl;
+    std::cout <<"Zero's -- " <<zero <<std::endl;
+
     return 0;
 }
