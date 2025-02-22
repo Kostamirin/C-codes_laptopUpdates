7ccadff310df547a9660d7bdec3c592426bb1de5
kley-muner_laptop <kerasin988@gmail.com>
2025-02-22 18:33:32 +0300

    University Grading System.cpp __update

---
+++ C++ (personal_works)/Calculator Versions/University Grading System.cpp
@@ -0,0 +1,39 @@
+// Created by kley-muner on 2/22/25.
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <ratio>
+
+using namespace std;
+void swap(vector<float>::iterator begin, vector<float>::iterator end);
+
+float calculateAverageGrade(std::vector<float> examScores) {
+    sort(examScores.begin(), examScores.end());
+    std::reverse(examScores.begin(), examScores.end());
+    vector<float> best;
+    for (const int i : 3)
+    {
+        best.push_back(examScores[i]);
+    }
+    auto it = find_if(examScores.begin(), examScores.end(), [](float num){return num > 90.0;});
+    if (it != examScores.end())
+    {
+        float answer = ((best[0], best[1], best[2])/3);
+        answer += (answer/100)*5;
+        return answer;
+    }
+    else
+    {
+        return ((best[0], best[1], best[2])/3);
+    }
+}
+
+int main()
+{
+    std::vector<float> examScores = {85.5, 92, 88, 79.5};
+    float done = calculateAverageGrade(examScores);
+    std::cout <<done <<std::endl;
+    return 0;
+}
+
+
