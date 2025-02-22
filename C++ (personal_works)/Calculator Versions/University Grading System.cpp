4a8a4239d7f639377f4c3eaeb37cdd632eb48299
kley-muner_laptop <kerasin988@gmail.com>
2025-02-22 18:38:16 +0300

    University Grading System.cpp __update Doesn't work

--- C++ (personal_works)/Calculator Versions/University Grading System.cpp
+++ C++ (personal_works)/Calculator Versions/University Grading System.cpp
@@ -10,7 +10,7 @@ float calculateAverageGrade(std::vector<float> examScores) {
     sort(examScores.begin(), examScores.end());
     std::reverse(examScores.begin(), examScores.end());
     vector<float> best;
-    for (const int i : 3)
+    for (int i; i < 3; i++)
     {
         best.push_back(examScores[i]);
     }
